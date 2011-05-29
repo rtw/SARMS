d8e039eb3a05eca9c0d5e366192be5e1b0d60d8d
rtw1981 <rtw1981@gmail.com>
2011-05-29 15:54:59 +1000

    added class diagram

--- Sarms.Domain/Administrator.cs
+++ Sarms.Domain/Administrator.cs
@@ -1,6 +1,14 @@
-﻿namespace Sarms.Domain
+﻿using System.Collections.Generic;
+
+namespace Sarms.Domain
 {
-    internal class Administrator : Account
+    public class Administrator : Account
     {
+        public IList<Report> Reports { get; set; }
+
+        public Administrator()
+        {
+            Reports = new List<Report>();
+        }
     }
 }
